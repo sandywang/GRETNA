bf4422597f23d770416306fbfea84964cf2c093e
sandywang <sandywang.rest@gmail.com>
2013-01-28 16:17:39 +0800

    Read 4D data not split

--- gretna_fc.m
+++ gretna_fc.m
@@ -6,19 +6,28 @@ function gretna_fc(DataList ,  LabMask , OutputName)
     Mask=reshape(Mask , [] , 1);
     Node=max(Mask , [] ,  1);
     TimeCourse=zeros(TimePoint , Node);
+    
+    Volume=spm_read_vols(cell2mat(P));
+    Volume=reshape(Volume , [] , TimePoint);
+    
+    
     for i=1:Node
-        Pos= Mask==i;
-        OneNodeTC=zeros(size(find(Pos~=0) , 1) , TimePoint);
-        for t=1:TimePoint
-            Volume=spm_read_vols(P{t});
-            Volume=reshape(Volume , [] ,1 );
-            OneNodeTC(:,t)=Volume(Pos);
-        end
-        OneNodeTC=OneNodeTC';
+        Pos= find(Mask==i);
+        %OneNodeTC=zeros(size(Pos , 1) , TimePoint);
+        %for t=1:TimePoint
+        %    Volume=spm_read_vols(P{t});
+        %    Volume=reshape(Volume , [] ,1 );
+        %    OneNodeTC(:,t)=Volume(Pos);
+        %end
         
-        MeanTC=mean(OneNodeTC , 2);
+        OneNodeTC=Volume(Pos , :);
+        
+        MeanTC=mean(OneNodeTC , 1);
         TimeCourse(:,i)=MeanTC;
     end
     r=corrcoef(TimeCourse);
     r(isnan(r))=0;
+    [Path , File , Ext]=fileparts(OutputName);
+    save([Path , filesep , 'TimeCourse_' , File , Ext] ,...
+        'TimeCourse' , '-ASCII', '-DOUBLE','-TABS');
     save(OutputName , 'r' , '-ASCII', '-DOUBLE','-TABS');

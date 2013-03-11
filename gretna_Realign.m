d3ec49722c90cf8710672e63d871b7cbee2cbcc9
sandywang <sandywang.rest@gmail.com>
2013-03-11 17:41:28 +0800

    Fix a BUG when plot head motion

--- gretna_Realign.m
+++ gretna_Realign.m
@@ -6,7 +6,7 @@ function gretna_Realign(RealignBatch , HMLogDir , SubjName , EPIPath)
     spm_jobman('run' , RealignBatch);
     HMFile=gretna_GetNeedFile(EPIPath , 'rp_*' , SubjName);
     HM=load(HMFile{1});
-    HM(4:6) = HM(4:6).*180/pi;
+    HM(:,4:6) = HM(:,4:6).*180/pi;
     
     x=(1:size(HM , 1))';
     h=figure('Position',[1,1,1024,768]);
@@ -17,9 +17,9 @@ function gretna_Realign(RealignBatch , HMLogDir , SubjName , EPIPath)
     legend('x transtion','y transtion','z transtion','Location','Best');
     
     subplot(2,1,2),plot(x,HM(:,4),x,HM(:,5),x,HM(:,6));
-    title('Rotation');
+    title('Degree');
     xlabel('Time Point');
-    ylabel('Degree');
+    ylabel('Radian');
     legend('pitch','roll','yaw','Location','Best');
     saveas(h,[HMLogDir , filesep , SubjName , '.jpg']);
     close(h);

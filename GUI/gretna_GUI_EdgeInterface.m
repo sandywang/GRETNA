52389a4013b4fdb527e1bb2572ce17efd4dba80f
sandywang <sandywang.rest@gmail.com>
2016-03-06 19:46:06 +0800

    Output All P value in Edge Statistical Analysis without thresholding

--- GUI/gretna_GUI_EdgeInterface.m
+++ GUI/gretna_GUI_EdgeInterface.m
@@ -648,7 +648,6 @@ switch Type
 %             end
                     
             BMap=logical(TMap);
-            PMap(~BMap)=0;
             BMap=double(BMap);
             
             save(fullfile(OutputDir, [Prefix, '_T.txt']), 'TMap', '-ASCII', '-DOUBLE', '-TABS');
@@ -676,7 +675,6 @@ switch Type
         end
                     
         BMap=logical(TMap);
-        PMap(~BMap)=0;
         BMap=double(BMap);
         save(fullfile(OutputDir, [Prefix, '_T.txt']), 'TMap', '-ASCII', '-DOUBLE', '-TABS');
         save(fullfile(OutputDir, [Prefix, '_P.txt']), 'PMap', '-ASCII', '-DOUBLE', '-TABS');
@@ -727,7 +725,7 @@ switch Type
         PorQ=str2double(get(handles.PEntry, 'String'));
         if CorrectValue==1 %None
             Index=P<PorQ;
-            P(~Index)=0;
+            %P(~Index)=0;
             T(~Index)=0;
         elseif CorrectValue==2 %FDR
             PThr=gretna_FDR(P, PorQ);
@@ -736,14 +734,14 @@ switch Type
                 return
             end
             Index=P<PThr;
-            P(~Index)=0;
+            %P(~Index)=0;
             T(~Index)=0;
             fprintf('\n\tFDR: Done.\n');
         elseif CorrectValue==3 %Bonferroni
             N=length(P);
             PThr=PorQ/N;
             Index=P<PThr;
-            P(~Index)=0;
+            %P(~Index)=0;
             T(~Index)=0;
             fprintf('\n\tBonferroni: Done.\n');
         elseif CorrectValue==4 %NBS
@@ -769,7 +767,6 @@ switch Type
 %                     TMap=abs(TMap);
 %             end
             BMap=logical(TMap);
-            PMap(~BMap)=0;
             BMap=double(BMap);
             
             save(fullfile(OutputDir, [Prefix, '_T.txt']), 'TMap', '-ASCII', '-DOUBLE', '-TABS');
@@ -797,7 +794,6 @@ switch Type
         end
                     
         BMap=logical(TMap);
-        PMap(~BMap)=0;
         BMap=double(BMap);
         save(fullfile(OutputDir, [Prefix, '_T.txt']), 'TMap', '-ASCII', '-DOUBLE', '-TABS');
         save(fullfile(OutputDir, [Prefix, '_P.txt']), 'PMap', '-ASCII', '-DOUBLE', '-TABS');
